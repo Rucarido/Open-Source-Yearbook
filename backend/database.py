3ed4d780d77882a5971ada36f7e5afba6a54d972
Aakash <aakashhariram22@gmail.com>
2025-10-26 19:28:44 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,9 @@
 import os
 from pymongo import MongoClient
 
-#MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://lemon:2007aakash22@cluster0.nc9dhdl.mongodb.net/?appName=Cluster0")  # Set env var for security
+#MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://<dbname>:<password>@cluster0.nc9dhdl.mongodb.net/?appName=Cluster0")  # Set env var for security
 
-client = MongoClient("mongodb+srv://lemon:2007aakash22@cluster0.bbhijse.mongodb.net/?appName=Cluster0")
+client = MongoClient("mongodb+srv://<dbname>:<password>@cluster0.bbhijse.mongodb.net/?appName=Cluster0")
 db = client["yearbook_db"]  # Database name
+
 users_collection = db["users"]
